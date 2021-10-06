2a5cc8aaf8bc9bb5944052fd7f23760889ddbcf5
MasudRana924 <masud15-924@diu.edu.bd>
2021-10-06 08:30:25 -0600

    not found page edited

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import NotFoundImg from '../../images/404.jpg'
+import './NotFound.css'
 
 const NotFound = () => {
     return (
        <Container fluid>
-           <img src={NotFoundImg} className="img-fluid" alt="" />
+           <img src={NotFoundImg} className="img-fluid w-50 mx-auto " alt="" />
        </Container>
     );
 };
